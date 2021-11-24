97d165aaa1ddca1882e8a963a141050b9ddd570c
jdaviesfox <james.daviesfox@gmail.com>
2021-11-24 12:49:41 +1100

    Minor update

--- src/data.js
+++ src/data.js
@@ -1,36 +1,36 @@
 export const work = [{
         id: 1,
-        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/anywhere-travel.jpg?raw=true",
+        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/src/img/anywhere-travel.jpg?raw=true",
         link: "https://anywheretravel.com.au/"
     },
 
     {
         id: 2,
-        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/printplus.jpg?raw=true",
+        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/src/img/printplus.jpg?raw=true",
         link: "https://www.printplussolutions.com.au/"
     },
 
     {
         id: 3,
-        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/voxelate-3.jpg?raw=true",
+        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/src/img/voxelate-3.jpg?raw=true",
         link: "https://www.youtube.com/watch?v=jGtBjDQVVq4"
     },
 
     {
         id: 4,
-        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/generative.jpg?raw=true",
+        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/src/img/generative.jpg?raw=true",
         link: "https://www.youtube.com/watch?v=VGxlZf3s4No"
     },
 
     {
         id: 5,
-        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/moneycat.jpg?raw=true",
+        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/src/img/moneycat.jpg?raw=true",
         link: "https://youtu.be/3gs9lTYjdrM"
     },
 
     {
         id: 6,
-        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/confetti.jpg?raw=true",
+        img: "https://github.com/jdaviesfox/portfolio-2021/blob/main/src/img/confetti.jpg?raw=true",
         link: "https://youtu.be/zi7_MUoPaUQ"
     }
 
